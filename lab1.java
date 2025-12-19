cfdcc046bddad9a958b6351800354d6c1162b684
charanccs24-prog <charanc.cs24@bmsce.ac.in>
2025-12-19 08:40:59 +0530

    Create lab1.java

---
+++ lab1.java
@@ -0,0 +1,64 @@
+import java.util.Scanner;
+
+class QuadraticEquation {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter value of a: ");
+        double a = sc.nextDouble();
+
+        System.out.print("Enter value of b: ");
+        double b = sc.nextDouble();
+
+        System.out.print("Enter value of c: ");
+        double c = sc.nextDouble();
+
+        double discriminant = b * b - 4 * a * c;
+
+        if (discriminant > 0) {
+            double root1 = (-b + Math.sqrt(discriminant)) / (2 * a);
+            double root2 = (-b - Math.sqrt(discriminant)) / (2 * a);
+
+            System.out.println("Two real solutions:");
+            System.out.println("Root 1 = " + root1);
+            System.out.println("Root 2 = " + root2);
+
+        } else if (discriminant == 0) {
+            double root = -b / (2 * a);
+
+            System.out.println("One real solution:");
+            System.out.println("Root = " + root);
+
+        } else {
+            System.out.println("No real solutions (Discriminant is negative)");
+        }
+
+        sc.close();
+    }
+}
+public static void main(String[] args) {
+
+    Scanner sc = new Scanner(System.in);
+
+    System.out.print("Enter value of a: ");
+    double a = sc.nextDouble();
+
+    System.out.print("Enter value of b: ");
+    double b = sc.nextDouble();
+
+    System.out.print("Enter value of c: ");
+    double c = sc.nextDouble();
+
+    double discriminant = b * b - 4 * a * c;
+
+    if (discriminant > 0) {
+        System.out.println("Two real solutions");
+    } else if (discriminant == 0) {
+        System.out.println("One real solution");
+    } else {
+        System.out.println("No real solutions");
+    }
+
+    sc.close();
+}
