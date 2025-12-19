22586e08d032614d6155b5026b652030ae262e9c
charanccs24-prog <charanc.cs24@bmsce.ac.in>
2025-12-19 09:26:12 +0530

    Update lab7.java

--- lab7.java
+++ lab7.java
@@ -1,1 +1,52 @@
+// Custom Exception Class
+class WrongAge extends Exception {
+    public WrongAge(String message) {
+        super(message);
+    }
+}
+
+// Base Class
+class Father {
+    int fatherAge;
+
+    Father(int age) throws WrongAge {
+        if (age < 0) {
+            throw new WrongAge("Father's age cannot be negative");
+        }
+        fatherAge = age;
+        System.out.println("Father age: " + fatherAge);
+    }
+}
+
+// Derived Class
+class Son extends Father {
+    int sonAge;
+
+    Son(int fatherAge, int sonAge) throws WrongAge {
+        super(fatherAge);
+
+        if (sonAge >= fatherAge) {
+            throw new WrongAge("Son's age must be less than Father's age");
+        }
+        if (sonAge < 0) {
+            throw new WrongAge("Son's age cannot be negative");
+        }
+
+        this.sonAge = sonAge;
+        System.out.println("Son age: " + sonAge);
+    }
+}
+
+// Main Class
+public class ExceptionInheritanceDemo {
+    public static void main(String[] args) {
+
+        try {
+            Son s = new Son(45, 20);   // valid case
+        } 
+        catch (WrongAge e) {
+            System.out.println("Exception caught: " + e.getMessage());
+        }
+    }
+}
 
